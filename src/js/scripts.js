38b51044ac67e2ad777356f8f632a0d8c423379a
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-01-03 15:18:08 -0300

    fix: menu bug | adjust html text/strcuture and css styling

---
+++ src/js/scripts.js
@@ -0,0 +1,13 @@
+const goToPortfolio = document.getElementById('option-portfolio')
+const goToHome = document.getElementById('option-home')
+const toggle = document.getElementById('toggle')
+
+
+goToPortfolio.addEventListener('click', () => {
+  toggle.click();
+})
+
+goToHome.addEventListener('click', () => {
+  toggle.click();
+})
+
